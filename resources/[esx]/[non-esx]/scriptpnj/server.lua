20edf617f65fb6d48717d89fc988aaabdb2666a9
ItsikNox <46906739+ItsikNox@users.noreply.github.com>
2019-09-23 22:23:03 +0200

    Stop Explosion

---
+++ resources/[esx]/[non-esx]/scriptpnj/server.lua
@@ -0,0 +1,11 @@
+local BlockedExplosions = {1, 2, 4, 5, 25, 32, 33, 35, 36, 37, 38}
+
+AddEventHandler("explosionEvent", function(sender, ev)
+    for _, v in ipairs(BlockedExplosions) do
+      if ev.explosionType == v then
+        CancelEvent()
+        --DropPlayer(source, 'Lua Execution/ModMenu')
+        return
+      end
+    end
+  end)
