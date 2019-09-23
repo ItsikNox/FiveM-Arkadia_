7135d1f61b3faa2d1d388b271185bb37ea14f07a
ItsikNox <46906739+ItsikNox@users.noreply.github.com>
2019-09-23 22:22:25 +0200

    Delete file

--- resources/[esx]/[ajouts]/esx_rpchat1/config.lua
+++
@@ -1,5 +0,0 @@
-Config        = {}
-Config.Locale = 'fr'
-
-Config.EnableESXIdentity = true -- only turn this on if you are using esx_identity and want to use RP names
-Config.OnlyFirstname     = true
