9f2fa1ab57770f9cf63bdbca2801ac30ad126560
ItsikNox <46906739+ItsikNox@users.noreply.github.com>
2019-09-24 00:14:19 +0200

    Update essentialmodeFRANCAIS.sql

--- essentialmodeFRANCAIS.sql
+++ essentialmodeFRANCAIS.sql
@@ -1422,6 +1422,20 @@ CREATE TABLE IF NOT EXISTS `phone_app_chat` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `user_sim`
+--
+
+CREATE TABLE IF NOT EXISTS `user_sim` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(555) NOT NULL,
+  `number` varchar(555) NOT NULL,
+  `label` varchar(555) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `phone_calls`
 --
 
