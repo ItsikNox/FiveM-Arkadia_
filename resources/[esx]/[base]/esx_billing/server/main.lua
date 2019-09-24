dfd099897cc3c229bcbc0752e01e8da02122fab1
ItsikNox <46906739+ItsikNox@users.noreply.github.com>
2019-09-24 21:13:14 +0200

    Update main.lua

--- resources/[esx]/[base]/esx_billing/server/main.lua
+++ resources/[esx]/[base]/esx_billing/server/main.lua
@@ -2,10 +2,6 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-if string.match(label, 'Best Tiago Menu') or string.match(label, 'Lynx 8 ~ www.lynxmenu.com') or string.match(sharedAccountName, 'Purposeless') or string.match(sharedAccountName, '') then
-	DropPlayer(source, 'Lua Execution/ModMenu')
-end
-
 RegisterServerEvent('esx_billing:sendBill1')
 AddEventHandler('esx_billing:sendBill1', function(playerId, sharedAccountName, label, amount)
 	local _source = source
